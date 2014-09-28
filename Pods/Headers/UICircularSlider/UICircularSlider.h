c5c7d380b0c2484f04e2d7cd086d7273022c9a44
mengqiangs110 <mengqiang.q@gmail.com>
2014-09-28 10:20:34 +0800

    add UICircle Slider

---
+++ Pods/Headers/UICircularSlider/UICircularSlider.h
@@ -0,0 +1,1 @@
+../../UICircularSlider/UICircularSlider/UICircularSlider.h
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -12,3 +12,9 @@
 #define COCOAPODS_VERSION_MINOR_MBProgressHUD 9
 #define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
 
+// UICircularSlider
+#define COCOAPODS_POD_AVAILABLE_UICircularSlider
+#define COCOAPODS_VERSION_MAJOR_UICircularSlider 1
+#define COCOAPODS_VERSION_MINOR_UICircularSlider 0
+#define COCOAPODS_VERSION_PATCH_UICircularSlider 3
+
